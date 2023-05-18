e01fe808f63e042b6719f75d2371f19ab39f2ab2
refnhaldy <refnhaldykristian@gmail.com>
2023-05-18 11:19:57 +0700

    Update url & Get list method

--- indodata.py
+++ indodata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Get Indonesia geographical data from Wikipedia
-url = 'https://id.wikipedia.org/wiki/Daftar_kota_di_Indonesia_menurut_jumlah_penduduk'
+url = 'https://id.wikipedia.org/wiki/Daftar_kota_di_Indonesia'
 
 try:
     table = pd.read_html(url)[0]
@@ -9,17 +9,18 @@ except Exception as e:
     print(f"Error retrieving data from {url}: {e}")
     exit()
 
+# Drop empty rows
+table = table.drop(index=10).reset_index(drop=True)
+
+# Clean data
+table['Kota'] = table['Kota'].str.replace('Kota ', '').str.replace('Administrasi ', '')
+table['Provinsi'] = table['Provinsi'].str.replace('Daerah Khusus Ibukota ', '').str.replace('Daerah Istimewa ', '')
+
 # Get city list
 city_list = list(table['Kota'])
-city_list = [city.replace('Kota ', '') for city in city_list]
 
 # Get province that in same lenght with cities for dictonary
 province_dict = list(table['Provinsi'])
-for i in range(len(province_dict)):
-    if province_dict[i] == 'Daerah Khusus Ibukota Jakarta':
-        province_dict[i] = 'Jakarta'
-    elif province_dict[i] == 'Daerah Istimewa Yogyakarta':
-        province_dict[i] = 'Yogyakarta'
 
 # Get province list
 provinces = list(set(province_dict))
